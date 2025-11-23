c1526f5267d8c7b3da6b809628cf4e36f3bcefd7
sriram936 <sriram06m@gmail.com>
2025-11-23 11:04:39 +0530

    feat: Make code  executable

--- backend/approach.py
+++ backend/approach.py
@@ -6,6 +6,7 @@ from indic_transliteration import sanscript
 from indic_transliteration.sanscript import transliterate
 import re
 import keyword
+import regex
 
 class Abs_Translator(ABC):
     @abstractmethod 
@@ -58,7 +59,7 @@ class Abs_Translator(ABC):
 
     '''
     #response = self.map_name_token(token, lang_pack) 
-    # is useful when we want to use some efficient algorithm in finding the correct map       
+    # is useful when we want to use some efficient algorithm in finding the correct map 
     '''    
     
 
@@ -66,6 +67,8 @@ class Abs_Translator(ABC):
     Current approach: Just take the input, and check if it has [a-zA-Z] if yes return is english (BUT IT FAILS IF LANGUAGE IS RELATED TO LATIN, LIKE SPANISH, FRENCH.....)
 
     After tokenization use regex on tokens to identify is_eng and is_Code_corpus
+    1) Not necessary => is_eng verified by lang
+    2)  Is code corpus verified by iskeyword() fn in keyword module
     '''
 
     '''
@@ -74,26 +77,28 @@ class Abs_Translator(ABC):
     I think this is efficient try this
     '''
 
-
+    '''
+    Start guess token part
+    '''
 
 
 class Translator(Abs_Translator):
     def __init__(self, lang: str, code: str):
         self.lang = lang
         self.code = code
-        lang_pack = self.get_dict(lang)
-        self.latest_code, self.unidentified = self.translate(self.code, lang_pack)
+        self.lang_pack = self.get_dict(lang)
+        self.latest_code, self.unidentified = self.translate(code, self.lang_pack)
         # self.translate_code(code, lang)
         return
     
 
-
+    #Working
     def get_dict(self, lang: str)-> dict:
         pack_path = ""
 
         if(lang.lower() == 'hindi'):
             print('hlo')
-            pack_path ="backend/language_packs/hindi.json"
+            pack_path ="convex/backend/language_packs/hindi.json"
             
         try:
             with open(pack_path, 'r') as lang_file:
@@ -110,13 +115,13 @@ class Translator(Abs_Translator):
         return {}
 
 
-
-    def untokenize_tokens(token_list: list)-> str:
+    #Working
+    def untokenize_tokens(self, token_list: list)-> str:
         return tokenize.untokenize(token_list)
 
 
-
-    def tokenize_code(code: str) -> list:
+    #Working
+    def tokenize_code(self,code: str) -> list:
         source_stream = io.StringIO(code)
         return tokenize.generate_tokens(source_stream.readline)
 
@@ -125,35 +130,36 @@ class Translator(Abs_Translator):
     '''
     Handle predicted case
     '''
+    #working
     def ask_user(self, token_to_verify: str, prediction: bool = False) -> str:
         if(prediction):
             print('Please verify the token')
             print(f'Is it \'\'{token_to_verify}\'\'')
             reply = int(input('If yes select 1 else 0'))
-            if(reply):
+            if(reply == 1):
                 return token_to_verify
             else:
-                response = self.ask_user(token_to_verify)
+                response = self.ask_user(token_to_verify, False)
                 return response
         else:
-            self.throw_error()
-            response = str(input('Please enter the right token')) 
+            # self.throw_error()
+            response = input('Please enter the right token') 
         return response
 
 
 
-    def map_name_token(token: str, lang_pack)-> str:
+    def map_name_token(self,token: str, lang_pack)-> str:
         changed_token = 'hlo'
         return changed_token
     
 
 
-    def guess_token(user_token: str)-> str:
-        predict = 'hlo'
+    def guess_token(self,user_token: str)-> str:
+        predict = user_token
         return predict 
 
 
-
+    #Not working
     def translate(self, source_code: str, lang_pack: dict) -> str :  #also returns a list of unidentified tokens
         """
         Flag to replace the token immediately following a backtick (`) with "transliterated word".
@@ -182,14 +188,13 @@ class Translator(Abs_Translator):
                     2) Use ascii of the corresponding characters range with regex to check if word belong to user mentioned language or not
                     3) Also store if the errors or  unidentified words in a seperate list to throw to user easily
                     '''
-                    pattern = r"\b\p{L}+\b"
-                    match_result = re.search(pattern, transliterate_string)
+                    pattern = r"\p{L}+"
+                    match_result = regex.search(pattern, transliterate_string)
 
                     if(bool(match_result)):
                         new_string = transliterate(transliterate_string, sanscript.DEVANAGARI, sanscript.ITRANS).lower()
                     else:
                         new_string = token_string
-
                     is_next_name = False
                 else :
                     if(keyword.iskeyword(token_string)):
@@ -197,13 +202,15 @@ class Translator(Abs_Translator):
                     elif token_string in lang_pack:
                         new_string = lang_pack[token_string]
                     else:
-                        pred = self.guess_token(token)
+                        pred = self.guess_token(token_string)
                         '''
                         1) It verifies predicted value and also corrects from the users response if the prediction is false
                         2) Use unidentified tokens for correcting and training the model                        
                         '''
-                        new_string = self.ask_user(pred, predicted=True)
+                        print("Dude its from the loop")
+                        new_string = self.ask_user(pred, prediction=True)
                         unidentified_tokens.append(token)
+                        # new_string = token_string 
             else:
                 new_string = token_string
 
@@ -273,7 +280,7 @@ class Translator(Abs_Translator):
 
 
 python_code = '''
-आयात random
+आयात `random
 
 परिभाषा `संख्या_जांच (`संख्या):
     अगर `संख्या > 10:
@@ -281,7 +288,7 @@ python_code = '''
     वरना:
         वापस झूठ
 
-`यादृच्छिक_संख्या = random.randint (1 , 20)
+`यादृच्छिक_संख्या = `random.randint (1 , 20)
 छापो (f"यादृच्छिक संख्या है: { `यादृच्छिक_संख्या }")
 
 अगर `संख्या_जांच (`यादृच्छिक_संख्या) है सच:
@@ -296,7 +303,7 @@ python_code = '''
 
 कोशिश:
     `परिणाम = 10 / 0
-छोड़कर ZeroDivisionError:
+छोड़कर `ZeroDivisionError:
     छापो (" ")
 अंततः:
     छापो (" ")
@@ -304,7 +311,8 @@ python_code = '''
 
 
 
-trans = Translator('Hindi', python_code)
-trans.get_dict()
-trans.latest_code
-trans.unidentified
+trans = Translator('hindi', python_code)
+print(trans.get_dict('hindi'))
+print(trans.latest_code)
+print(trans.unidentified)
+print(trans.ask_user("hello",True))
